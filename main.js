fba43b668be0ebaddcbea05bc3767d0a2d3773d5
5Wymiar <mikolaj.fiedor123@gmail.com>
2020-04-26 20:50:25 +0200

    Exe from JS course

---
+++ main.js
@@ -0,0 +1,68 @@
+// const promo = '10%';
+
+// switch (promo) {
+//     case '10%':
+//         console.log('Dzis jest 10% taniej')
+//         break;
+
+//     case '20%':
+//         console.log('Dzis jest 20% taniej') 
+//         break;
+
+//     case '30%':
+//          console.log('Dzis jest 30% taniej')
+//         break;        
+//     default:
+//         console.log('Dziś nie ma zniżek ;(')
+//         break;
+// }
+
+
+
+//first method
+// const x = 10;
+// console.log( (x%2 === 0) ? 'Jest podzielne' : 'Nie jest podzielne');
+
+//second method
+// const x = 10;
+// const check = (x%2 === 0) ? 'Jest podzielne' : 'Nie jest podzielne';
+
+// console.log(check);
+
+let x = 50;
+let $huge; 
+
+if ( x >= 100){
+
+   $huge = ('X jest wiekszy');
+
+} else if ( x < 100 && x > 30){
+
+   $huge = ('x jest średni');
+
+}else {
+   $huge = ('x jest mały');
+
+}
+
+console.log($huge.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
