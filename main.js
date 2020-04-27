d18976e4577d430955b98d1ace971b7334a9a4db
5Wymiar <mikolaj.fiedor123@gmail.com>
2020-04-27 17:11:14 +0200

    Next step in Js course petle

--- main.js
+++ main.js
@@ -29,23 +29,219 @@
 
 // console.log(check);
 
-let x = 50;
-let $huge; 
+// let x = 50;
+// let $huge; 
 
-if ( x >= 100){
+// if ( x >= 100){
 
-   $huge = ('X jest wiekszy');
+//    $huge = ('X jest wiekszy');
 
-} else if ( x < 100 && x > 30){
+// } else if ( x < 100 && x > 30){
 
-   $huge = ('x jest średni');
+//    $huge = ('x jest średni');
 
-}else {
-   $huge = ('x jest mały');
+// }else {
+//    $huge = ('x jest mały');
 
-}
+// }
 
-console.log($huge.toUpperCase());
+// console.log($huge.toUpperCase());
+
+//ĆWICZENIA PĘTLA FOR + TABLICE
+
+// const cars = ['tesla', 'mercedes', 'mazda', 'jaguar', 'vg'];
+
+// for (let i = 0; i < cars.length; i++) {
+//    console.log(cars[i]);
+   
+// }
+
+
+// const $countries = ['france', 'poland', 'netherlands', 'deutchlands', 'italy'];
+
+// for ( let i = 0; i <$countries.length; i++){
+//    console.log($countries[i]);
+// }
+
+
+// const $capitals = ['paris', 'warsow', 'amsterdam', 'berlin', 'rome'];
+
+// for (let i = 0; i <$capitals.length; i++){
+//    console.log($capitals[i]);
+// }
+
+// ĆWICZENIA PĘTLA WHILE
+
+// let i = 0;
+
+// while (i < 5) {
+//    console.log(`Liczba wynosi: ${i}`);
+//    i++ ;
+   
+// };
+
+//ĆWICZENIA PĘTLA DO WHILE
+
+// let i = 0;
+
+// do {
+//    console.log(`Przed inkrementacją ${i}`);
+//    i++;
+   
+//    console.log(`Po inkrementacją ${i}`);
+// } while (i < 2);
+
+
+// PĘTLA FOR OFF
+
+// const numbers = [2, 3, 10, 25, 55];
+
+// for (const number of numbers) {
+
+//    console.log(number * 3 );
+   
+// }
+
+
+// PĘTLA FOREACH
+
+// const numbers = [1, 3, 4, 5, 63 ];
+
+// const newNumbers = numbers.forEach( el => console.log(el * 2));
+
+
+// const citys = ['london', 'moscow', 'denver', 'rio'];
+
+// for( let i = 0; i < citys.length; i++  ){
+
+//    console.log(`Jedziemy do ${citys[i]}`);
+
+// }
+
+
+// let x = 0;
+
+// while ( x <= 10) {
+
+//    console.log(`X jest równy ${[x]}`);
+//    x = x +2;
+   
+// }
+// let x = 20;
+
+// do{
+//    x = x - 3;
+// }while (x > 0 ) 
+//    console.log(`X wynosi ${[x]}`);
+
+const numbers = [5,8,10,23,44];
+
+for(const number of numbers){
+   if ( number % 2 === 0 ){
+      console.log(`${number}  jest podzielna przez 2`)
+   } else {
+      console.log(`${number}  nie jest podzielna przez 2`)
+   }   
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
