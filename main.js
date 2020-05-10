523659bedc1cd13b096e535cb43c4838ec827876
5Wymiar <mikolaj.fiedor123@gmail.com>
2020-05-10 13:11:29 +0200

    Watching overment films

--- main.js
+++ main.js
@@ -519,6 +519,8 @@ const cars = 'mercedes, audi, bmw, mustang, nissan';
 
 //Api, hoisting
 
+//Overment watching film about function compose and Scope, next.js, command line oxlinux and api.
+
 
 
 
